6a07e09bc8783e61b1b05e2442498b8bf5c9c914
VALDEZ R. <renzv753@gmail.com>
2024-10-16 08:20:41 +0800

    Create Array Activity.js

---
+++ Array Activity.js
@@ -0,0 +1,10 @@
+const favSingers = ["Celine Dion", "Parokya ni Edgar", "Renz Verano"];
+console.log(favSingers[0]); 
+
+const favNumbers = [10, 13, 22, 25];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); // "string"
+console.log(mixedArr[1]); // ["otherarray"]
+console.log(mixedArr[2]); // 123
+console.log(mixedArr[3]); // true
